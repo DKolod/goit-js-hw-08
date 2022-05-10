67dfca7d3bae09b6ee7636cfa43bfeadc116b0b7
Dmitriy Kolodchenko <dima.kolod@gmail.com>
2022-05-10 14:52:07 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import vimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+const onPlay = function (data) {
+
+ const savedTime = localStorage.getItem("videoplayer-current-time");
+    if (savedTime) {
+     player.setCurrentTime(Number(savedTime));
+  }
+};
+
+const onTimeUpdate = function(data) {
+localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.on('play', onPlay);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
